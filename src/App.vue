d2180fbac892738833dce6c2f5d579347a0852b0
HE77XTL <he77505357@163.com>
2018-05-08 23:39:09 +0800

    复杂对象JSON化上传至cleancloud

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
   <div id="app">
     <router-view v-bind:data="data"></router-view>
     {{data.resumer}}
+    <button v-on:click="saveData()">保存</button>
   </div>
 </template>
 
 <script>
+import AV from 'leancloud-storage'
 export default {
   name: 'App',
   data(){
@@ -39,6 +41,35 @@ export default {
           ]
         }
       }
+    }
+  },
+  methods: {
+    saveData: function(){
+     console.log(111)
+     var json_data=JSON.stringify(this.data)
+
+  // 第一个参数是 className，第二个参数是 objectId
+  var todo = AV.Object.createWithoutData('Todo', '5af1b8869f54545462c006d4');
+  // 修改属性
+  todo.set('name', '每周工程师会议，本周改为周三下午3点半。');
+  // 保存到云端
+  todo.save();
+
+
+  // // 声明类型
+  // var Todo = AV.Object.extend('Todo');
+  // // 新建对象
+  // var todo = new Todo();
+  // // 设置名称
+  // todo.set('name',json_data);
+  // // 设置优先级
+  // todo.set('priority',1);
+  // todo.save().then(function (todo) {
+  //   console.log('objectId is ' + todo.id);
+  //   console.log(todo)
+  // }, function (error) {
+  //   console.error(error);
+  // });
     }
   }
 }
